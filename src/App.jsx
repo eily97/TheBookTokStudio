20b045e983242391c3eb08dc352fe04b1d81c5f5
eily97 <yarenpekgil97@gmail.com>
2026-06-21 00:33:05 +0300

    Add signInEmail prop to component

--- src/App.jsx
+++ src/App.jsx
@@ -229,6 +229,10 @@ function AppContent() {
         onProfileClick={() => { setPage("profile"); profileHook.load(); bookHook.loadReadingList(); }}
         onSignOut={signOut}
         onSignIn={signIn}
+        onSignOut={signOut}
+        onSignIn={signIn}
+        onSignInEmail={signInWithEmail}
+        onAdminClick={openAdmin}
         onAdminClick={openAdmin}
       />
       {showPWA && (
